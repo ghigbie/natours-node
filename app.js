fb64913eb7fb8e2ace587e780e37301659fca557
ghigbie <gehigbie@gmail.com>
2019-10-20 19:26:06 -0700

    Add new get endpoint

--- app.js
+++ app.js
@@ -21,8 +21,39 @@ app.get('/api/v1/tours', (req, res, next) => {
         });
 });
 
-app.post('/api/v1/tours', (req, res, next) => {
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(tour => tour.id === id);
+    res
+        .status(200)
+        .json({
+            status: 'sucess',
+            data: {
+                tour
+            }
+        });
+})
 
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body);
+    const newId = tours[tours.length-1] +1;
+    const newTour = Object.assign({id: newId}, req.body); //creates a new object by merging two objects together
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, 
+        JSON.stringify(tours), err => {
+            if(err){ 
+                console.log(`Error: ${err}`)
+            }else{
+                res
+                    .status(201)
+                    .json({
+                        status: 'success',
+                        data: {
+                            tour: newTour
+                        }
+                    })
+            }
+    });
 });
 
 
