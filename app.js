24a836e282b9b4519724246f7215b25b0c6816d7
ghigbie <gehigbie@gmail.com>
2019-10-20 19:34:11 -0700

    Add conditional rendering

--- app.js
+++ app.js
@@ -24,14 +24,20 @@ app.get('/api/v1/tours', (req, res, next) => {
 app.get('/api/v1/tours/:id', (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find(tour => tour.id === id);
-    res
-        .status(200)
-        .json({
-            status: 'sucess',
-            data: {
-                tour
+
+    if(id > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }else{
+    res.status(200).json({
+        status: 'sucess',
+        data: {
+            tour
             }
         });
+    }
 })
 
 app.post('/api/v1/tours', (req, res) => {
