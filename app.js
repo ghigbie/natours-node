0910af6272aa71ac218fa9efa6153448b46da2e0
ghigbie <gehigbie@gmail.com>
2019-10-20 18:14:58 -0700

    Add new file system method

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.get('/api/v1/tours', (req, res, next) => {
+
+});
 
 
 app.listen(PORT , () => {
