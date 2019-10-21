ee8a5ca2a9bcecde7bd5445690a909a50be985f6
ghigbie <gehigbie@gmail.com>
2019-10-20 20:13:18 -0700

    Add conditional res

--- app.js
+++ app.js
@@ -63,6 +63,12 @@ app.post('/api/v1/tours', (req, res) => {
 });
 
 app.patch('/api/v1/tours/:id', (req, res, next) => {
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
     res.status(200).json(
         {
             status: 'success',
